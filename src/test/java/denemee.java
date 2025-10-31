a17f9ae7b9e6300a4eaea86b43ae3f624dafd1eb
lazKorsan <ahmeteldes2002@gmail.com>
2025-10-31 16:38:37 +0300

    say hello

--- src/test/java/denemee.java
+++ src/test/java/denemee.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class denemee {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        // ikininci aşama
+        // settings cısmına git
+        /*
+        collabrates kısmına git
+        sayfanın altında add to people kısmını bul
+        mail adreslerini gir
+        davetiye gönder
+        pending den collabartaor olmasını bekle
+
+
+         */
+    }
 }
